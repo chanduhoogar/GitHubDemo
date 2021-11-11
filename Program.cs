ecc59fc0677253f0ad651b632717e369e03e2f64
Chandana M <Chandana.M@visionetsystems.com>
2021-11-11 18:07:24 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GitHubDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 2;
+            if (i == 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
